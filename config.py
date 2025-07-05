2655d50910e8481a6cd9e524254fd4b82fe0ffc3
Reactorchanin <anemo2@mail.ru>
2025-07-05 16:33:18 +0300

    Пытаюсь починить склад

--- config.py
+++ config.py
@@ -8,9 +8,8 @@ load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
 # Файл для хранения статистики
-import os
 data_dir = os.getenv('RAILWAY_PERSISTENT_STORAGE_PATH', '.')
 STATS_FILE = os.path.join(data_dir, 'stats.json')
 
 # Логирование
-LOG_FILE = 'bot.log' 
+LOG_FILE = 'bot.log'
